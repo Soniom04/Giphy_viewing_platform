20b220d96c647038692e6cadca66a234cfe1ac95
Soniom04 <soniom.s2020@gmail.com>
2024-09-03 01:14:54 +0530

    Chnage Made

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Tag from "./components/Tag";
 
 function App() {
   return (
-    <div className="flex flex-col w-full background pb-5">
+    <div className="flex flex-col w-full background min-h-screen">
         <h1 className="bg-white rounded-lg mt-7 text-center mx-7 py-2 text-3xl font-bold">RANDOM GIFS</h1>
         <div className="flex flex-col"> 
             <Random/>
